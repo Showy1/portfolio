e61b80bc41a38dde8da0ecb1ecec9f46aca32cdf
Showy <>
2020-07-14 17:44:34 +0900

    initial

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+  "use strict";
+
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+  $('#hamburger').click(function(){
+    $("body").addClass("show");
+  })
+  $('#close, a[href^="#"]').click(function(){
+    $("body").removeClass("show");
+  })
+});
